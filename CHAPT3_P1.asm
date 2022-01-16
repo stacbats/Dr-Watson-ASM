ffbf432f5957d60acdaff589aa35958eaa8e437b
Stacy Bates (stacbats) <44994840+stacbats@users.noreply.github.com>
2022-01-16 20:58:50 +0000

    Add files via upload

---
+++ CHAPT3_P1.asm
@@ -0,0 +1,25 @@
+BasicUpstart2(main)
+// Chapter 3  Addressing and Screen Outputting:
+// Working on using the X & Y registers to manipulate and Animate
+
+// using STX, if X contained 100 and the Accumulator 90, the instruction stx 1024 will put a diamond 
+// into 1024 plus 100. Using the Increment instruction, this enables the location on screen to be indexed
+//   Program 3.1
+
+ 
+.const ScreenMem     = $0400
+.const ColorMem      = $d800
+
+
+* = $828 "Start Address"   // Poke 2088
+
+main:
+    ldx #100        // 2 bytes: load 100 into X
+L1: lda #90         // 2 bytes: Load 90 into A (our diamond)
+    sta 1023,x   // 3 bytes: Outputs the diamond at address (1023+x)
+    lda #1          // 2 bytes: Load Accumulator with 1
+    sta 55295,x    // 3 bytes: Ensure the colour is white for colour Mem
+    dex             // 1 byte:  decrement the X value  (243 / $f3)
+    bne L1          // 2 byte: Branch if not equal. The book says BNE 243 
+
+    rts  
---
+++ CHAPT3_P3.asm
@@ -0,0 +1,25 @@
+// Chapter 3  Addressing and Screen Outputting:
+// Working on using the X & Y registers to manipulate and Animate
+
+
+
+
+* = $828 "Start Address"   // Poke 2088
+
+main:
+    ldx #0
+    ldy #32
+    sty 900
+    lda #90
+    sta 901
+L1: sta 1024,x
+    lda #1
+    sta 55296,x
+    tya
+    sta 1023,x
+    lda 901
+    inx
+    bne L1
+
+
+    rts
