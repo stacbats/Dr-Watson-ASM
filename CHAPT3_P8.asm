baa03d793a09ba075ba02c281eb06baf8707bb5f
Stacy Bates (stacbats) <44994840+stacbats@users.noreply.github.com>
2022-01-19 21:26:38 +0000

    Add files via upload

--- CHAPT3_P8.asm
+++ CHAPT3_P8.asm
@@ -9,31 +9,32 @@ BasicUpstart2(main)
 * = $828 "Start Address"   // Poke 2088
 
 main:
-    ldy #0          // 2 cycles:
-    lda #90         // 2 cycle:           // set up the diamond
-    sta 900         // 4 cycle:            // STORE IT in 900
-    lda #1          // 2 cycle:             // Make them White
-    sta 901         // 4 cycle:          // STORE IT IN 901
-    lda #32         // 2 cycle:        // set up the blank
-    sta 902         // 4 cycle:        // STORE IT IN 902
-    lda 900         // 4 cycle:
-S1: sta 1024,y      // 5 cycle:
-    lda 901         // 4 cycle:
-    sta 55296,y     // 5 cycle:
-    sty 903         // 4 cycle:
+    ldy #0          // 2 bytes:
+    lda #90         // 2 bytes:           // set up the diamond
+    sta 900         // 3 bytes:            // STORE IT in 900
+    lda #1          // 2 bytes:             // Make them White
+    sta 901         // 3 bytes:          // STORE IT IN 901
+    lda #32         // 2 bytes:        // set up the blank
+    sta 902         // 3 bytes:        // STORE IT IN 902
+S1: lda 900         // 4 bytes:
+    sta 1024,y      // 3 bytes:
+    lda 901         // 3 bytes:
+    sta 55296,y     // 3 bytes:
+L2: sty 903         // 3 bytes:
 
-L2: ldy L1          // 4 cycles:LOOP 1 = OUTER LOOP 15  (loop forward)
-L3: ldx L2          // 4 cycles:LOOP 2 = INNER LOOP 250  (loop back)
+L3: ldy L1          // 3 bytes:LOOP 1 = OUTER LOOP 15  (loop forward)
+    ldx L2          // 3 bytes:LOOP 2 = INNER LOOP 250  (loop back)
 
-L4: dex             // 2 cycle:
-    bne L3          // 2 cycles:LOOP 3 = Count down 250 (253)
-    dey             // 2 cycle:
-    bne L4          // 2 cycle: LOOP 4: = Count down 15 (248)
+    dex             // 1 byte:
+    bne L3          // 2 bytes:LOOP 3 = Count down 250 (253)
+    dey             // 1 bytes:
+L1: bne L2          // 2 bytes: LOOP 4: = Count down 15 (248)
 
-L1: ldy 903         // 4 cycle:
-    lda 902         // 4 cycle:
-    sta 1024, y     // 3 cycle:
-    iny             // 2 cycle:
-    bne S1          // 2 cycle: LOOP 5 = Keep looping until done. Branch back 219 
+    ldy 903         // 3 bytes:
+    lda 902         // 3 bytes:
+    sta 1024, y     // 3 bytes:
+    iny             // 1 bytes:
+   
+    bne S1          // 2 bytes: LOOP 5 = Keep looping until done. Branch back 219 
     
-    rts
+    rts  
